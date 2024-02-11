a6ded55e0a19bdaa9674da6a5fa9439d9bd3a497
Arpit yadav <115612258+Arpityadavvvv@users.noreply.github.com>
2024-02-11 18:17:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ const App = () => {
   
   
   return ( <div>
-     <div className="w-[100vw] h-[100vh] flex flex-col justify-center items-center bg-gray-200 ">
+     <div className="w-[100vw] h-[100vh]  flex flex-col justify-center items-center bg-gray-200 ">
       <h1 className="text-center text-4xl font-bold ">OUR TESTIMONIALS </h1>
-      <div className=" bg-violet-400 h-[4px] w-[40px] mt-1"></div>
+      <div className=" bg-violet-400 h-[4px] w-[40px] sm:h-[2px] sm:w-[20px] mt-1"></div>
 
       <Testimonial reviews = {reviews}></Testimonial>
      </div>
